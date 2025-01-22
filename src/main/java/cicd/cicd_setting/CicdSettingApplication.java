d743a2326d82da7dfd5c9772ba1e733bc461ce28
Suhun0331 <rlatngns6046@naver.com>
2025-01-22 11:13:00 +0900

    First setting

---
+++ src/main/java/cicd/cicd_setting/CicdSettingApplication.java
@@ -0,0 +1,13 @@
+package cicd.cicd_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdSettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cicd/cicd_setting/CicdSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd.cicd_setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
